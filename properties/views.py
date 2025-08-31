4c97e205ff08f2545259453dcdac9079aefc032d
prevyne <prevyneoketch6@gmail.com>
2025-08-31 22:35:00 +0300

    fix: task1

--- properties/views.py
+++ properties/views.py
@@ -1,14 +1,14 @@
-# properties/views.py
-
 from django.http import JsonResponse
-from .utils import get_all_properties
+from django.views.decorators.cache import cache_page
+from .models import Property
 import json
 from django.core.serializers import serialize
 
+@cache_page(60 * 15)
 def property_list(request):
     """
-    A view that returns all properties using low-level queryset caching.
+    A view that returns all properties, cached at the view level.
     """
-    properties = get_all_properties()
+    properties = Property.objects.all()
     data = serialize('json', properties)
     return JsonResponse(json.loads(data), safe=False)
